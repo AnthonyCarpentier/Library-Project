1cc3cacf79ee2c5edbf36e0fd71179ca73d2a6c9
Anthony Carpentier <AnthonyEvanCarpentier@gmail.com>
2020-12-31 20:14:04 -0800

    Added my code from the previous lesson's Book example, to give me a springboard to work from.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-// test
+let readReport;
+
+function books(title, author, pages, read, info) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+	if (read === 1) {
+		readReport = `already read`;
+	} else {
+		readReport = `not read yet`;
+	}
+	this.info = function (string) {
+	return (`` + title + ` by ` + author + `, ` + pages + ` pages, `	+ readReport);
+}
+}
+
+let hobbit = new books(`The Hobbit`, `J.R.R. Tolkien`, 295, 0)
+
+console.log(hobbit.info());
